b3ba57aabc3c1bcde9847102dd414aa644cec142
Irving Daniel Reyes Mayo <irvingdaniel.reyes@bbva.com>
2023-11-07 02:06:48 -0600

    Fin de cap. 109 Simplificando con .map

--- intro-dom/main.js
+++ intro-dom/main.js
@@ -1,16 +1,6 @@
 const todos = [];
 
 window.onload = () =>{
-    //const parrafo = document.getElementById('text');
-
-    //innerHTML e innerText funcionan igual para obtener
-    //el texto de un elemento html como <p></p>
-    //console.log(parrafo);
-
-    /*
-    innerHTML:
-    Para agregar elementos HTML a nuestro documento*/
-    //parrafo.innerHTML = '<li>elemento 1</li><li>elemento 2</li>';
     const form = document.getElementById('todo-form');
     form.onsubmit = (e) =>{
         e.preventDefault(); //hace que la pagina no refresque
@@ -19,9 +9,7 @@ window.onload = () =>{
         todo.value='';
         todos.push(todoText);
         const todoList = document.getElementById('todo-list');
-        todoList.innerHTML = '';
-        for(let i=0;i < todos.length;i++){
-            todoList.innerHTML += '<li>'+todos[i]+'</li>';
-        }
+        const todosTemplate = todos.map(t =>'<li>' + t + '</li>');
+        todoList.innerHTML = todosTemplate.join('');
     }
 }
