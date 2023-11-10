10d3e9f02903ff553af7fa27ab1d976c56b688c0
idrey <i.r3y.01@gmail.com>
2023-11-10 01:22:53 -0600

    Se sube fin de cap. 127 Refactorizando los endPoint

---
+++ api/api.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const user = require('./user.controller');
+const User = require('./user.controller');
+const app = express();
+const port = 3000;
+
+app.get('/', User.list);
+app.post('/', User.create);
+app.get('/:id', User.get);
+app.put('/:id', User.update);
+app.patch('/:id', User.update);
+app.delete('/:id', User.destroy);
+
+app.listen(port,()=>{
+    console.log('Arrancando la aplicación');
+});
+
